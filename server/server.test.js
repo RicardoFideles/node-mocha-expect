17f874f996e1dc822b399bbc55bfc9218fad7b1b
Ricardo Fideles <ricardo@ricardofideles.com.br>
2017-07-05 17:13:19 -0300

    organizing tests with describes

--- server/server.test.js
+++ server/server.test.js
@@ -3,32 +3,43 @@ const expect = require('expect');
 
 var app = require('./server').app;
 
-it('It Should return hello world responde', (done) => {
-    request(app)
-        .get('/')
-        .expect(404)
-        .expect((res) => {
-            expect(res.body).toInclude(
-                    {
-                        name : 'Todo app v1',
-                        error : 'Page not found'
-                    }
-            )
-        })
-        .end(done);
-});
+describe('Server - Routes ', () => {
 
-it('It Should return my user object in responde', (done) => {
-    request(app)
-        .get('/users')
-        .expect(200)
-        .expect((res) => {
-            expect(res.body).toInclude(
-                    {
-                        firtname : 'Ricardo',
-                        age: 32
-                    }
-            )
-        })
-        .end(done);
+    describe('#GET', () => {
+        describe('/', () => {
+            it('It Should return hello world responde', (done) => {
+                request(app)
+                    .get('/')
+                    .expect(404)
+                    .expect((res) => {
+                        expect(res.body).toInclude(
+                            {
+                                name: 'Todo app v1',
+                                error: 'Page not found'
+                            }
+                        )
+                    })
+                    .end(done);
+            });
+        });
+
+        describe('/users', () => {
+            it('It Should return my user object in responde', (done) => {
+                request(app)
+                    .get('/users')
+                    .expect(200)
+                    .expect((res) => {
+                        expect(res.body).toInclude(
+                            {
+                                firtname: 'Ricardo',
+                                age: 32
+                            }
+                        )
+                    })
+                    .end(done);
+            });
+        });
+
+    });
+
 });
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -3,60 +3,68 @@ const expect = require('expect');
 const utils = require('./utils');
 
 
-it('should add two numbers', () => {
-    var res = utils.add(33,11);
-    expect(res).toBe(44).toBeA('number');
+describe('Utils', () => {
+
+    describe('#add', () => {
+        it('should add two numbers', () => {
+            var res = utils.add(33, 11);
+            expect(res).toBe(44).toBeA('number');
+        });
+    });
+
+    it('should square a number', () => {
+        var res = utils.square(3);
+        expect(res).toBe(9).toBeA('number');
+    });
+
+    it('Should validate add async', (done) => {
+        utils.asyncAdd(4, 3, (sum) => {
+            expect(sum).toBe(7).toBeA('number');
+            done();
+        });
+    });
+
+    it('should asyncSquare a number', (done) => {
+        utils.asyncSquare(3, (res) => {
+            expect(res).toBe(9).toBeA('number');
+            done();
+        });
+    });
+
 });
 
-it('should square a number', () => {
-    var res = utils.square(3);
-    expect(res).toBe(9).toBeA('number');
-});
 
-it('should expect some values', () => {
-    expect(12).toNotBe(11);
-    expect(12).toBe(12);
-    expect({name:"Ricardo"}).toEqual({name:"Ricardo"});
-    expect({name:"RicardoF"}).toNotEqual({name:"Ricardo"});
-    expect([2,3,4]).toInclude(3);
-    expect([2,3,4]).toExclude(5);
-    expect({name:"Ricardo", age:32, location:"Rio de Janeiro"}).toInclude({age:32});
-    expect({name:"Ricardo", age:32, location:"Rio de Janeiro"}).toExclude({age:33});
+describe('Playground', () => {
+    it('should expect some values', () => {
+        expect(12).toNotBe(11);
+        expect(12).toBe(12);
+        expect({ name: "Ricardo" }).toEqual({ name: "Ricardo" });
+        expect({ name: "RicardoF" }).toNotEqual({ name: "Ricardo" });
+        expect([2, 3, 4]).toInclude(3);
+        expect([2, 3, 4]).toExclude(5);
+        expect({ name: "Ricardo", age: 32, location: "Rio de Janeiro" }).toInclude({ age: 32 });
+        expect({ name: "Ricardo", age: 32, location: "Rio de Janeiro" }).toExclude({ age: 33 });
 
-});
+    });
 
-it ('Should verify if firt name and last name are set', () => {
-    var user = {
-        age: 32,
-        location : "Rio de Janeiro"
-    };
-    var fullName = "Ricardo Fideles";
-    var user = utils.setName(user, fullName);
-    expect(user).toInclude(
-        {   
-            firstName: "Ricardo",
-            lastName: "Fideles",
-            age:32,
-            location:"Rio de Janeiro"
-        }
-    );
-    expect(user).toExclude({location:"EUA"});
-});
-
-it ('Should validate add async', (done) => {
-    utils.asyncAdd(4,3, (sum) => {
-        expect(sum).toBe(7).toBeA('number');
-        done();
+    it('Should verify if firt name and last name are set', () => {
+        var user = {
+            age: 32,
+            location: "Rio de Janeiro"
+        };
+        var fullName = "Ricardo Fideles";
+        var user = utils.setName(user, fullName);
+        expect(user).toInclude(
+            {
+                firstName: "Ricardo",
+                lastName: "Fideles",
+                age: 32,
+                location: "Rio de Janeiro"
+            }
+        );
+        expect(user).toExclude({ location: "EUA" });
     });
 });
-
-it('should asyncSquare a number', (done) => {
-    utils.asyncSquare(3, (res) => {
-         expect(res).toBe(9).toBeA('number');
-         done();
-    });
-});
-
 
 
 //Run custom scripts
